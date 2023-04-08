d390819d2fee798aa2e19389e904bdae2da208a1
dcnxx1 <alkisali1@hotmail.com>
2023-04-08 16:14:34 -0700

    dummy text added

--- app/App.tsx
+++ app/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>
-        {/* {data.setData.isLoading ? "isLoading now.." : data.setData.response} */}
+       default fummy text!
       </Text>
       <StatusBar style="auto" />
     </View>
