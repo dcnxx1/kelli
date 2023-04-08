511d894cd94d1f1c5586fde7a3a00577c67c206e
dcnxx1 <alkisali1@hotmail.com>
2023-04-08 05:16:43 -0700

    removed index.ts file from screens (only one screen)

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { SWRConfigFactory } from './src/config';
-import { Home } from './src/screens';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SWRConfigFactory } from "./src/config";
+import Home from "./src/screens/Home";
 
 export default function App() {
   return (
--- app/src/services/phero.ts
+++ app/src/services/phero.ts
@@ -1,11 +1,10 @@
 import { PheroClient } from "../phero.generated";
 
-
 const client = new PheroClient(fetch);
 
 async function main() {
-  const message = await client.helloWorldService.helloWorld('Jim')
-  console.log(message) // `Hi there, Alki`
+  const message = await client.helloWorldService.helloWorld("Jim");
+  console.log(message); // `Hi there, Alki`
 }
 
-main()
+main();
