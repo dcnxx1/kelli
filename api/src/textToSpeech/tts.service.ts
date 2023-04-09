213d096537e6ad82616896c251e3f84b5417a156
dcnxx1 <alkisali1@hotmail.com>
2023-04-09 03:40:42 -0700

    added endOfLine: auto to prettierrc to fix annoying red lines

--- api/src/textToSpeech/tts.service.ts
+++ api/src/textToSpeech/tts.service.ts
@@ -3,11 +3,11 @@ import { Polly } from 'aws-sdk';
 
 @Injectable()
 export class TTSService {
-  getDefaultTTS(): string {
-    return 'CHANGED AGAIN AGAIN';
-  }
+ getDefaultTTS(): string {
+  return 'CHANGED AGAIN AGAIN';
+ }
 
-  getSomeTTS(): string {
-    return 'SOMETHING AGAIN';
-  }
+ getSomeTTS(): string {
+  return 'SOMETHING AGAIN';
+ }
 }
