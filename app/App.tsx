6432b5340b17c8444a3f0d819bb7d584e00e2505
dcnxx1 <alkisali1@hotmail.com>
2023-04-10 04:54:26 -0700

    removed unused imports

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { useRequest } from "./src/shared/hooks";
+import { StyleSheet, View } from "react-native";
 import { Home } from "./src/screens";
 
 export default function App() {
-  
+
   return (
     <View style={homeStyle.App}>
       <Home />
