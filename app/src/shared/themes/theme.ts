10d8d15ab207d3ce9ad96c98be95d6fe32775fea
dcnxx1 <alkisali1@hotmail.com>
2023-04-10 05:00:22 -0700

    added colors

---
+++ app/src/shared/themes/theme.ts
@@ -0,0 +1,14 @@
+export default {
+  colors: {
+    kelliBright: "#FFDF00",
+    kelliLight: "#FFEE75",
+    kelliGold: "#ffba00",
+    kelli: "#ffd800",
+    kelliBrown: "#ef9b0f",
+    kelliBlack: "#100c08",
+    kelliGrey: "#c9c9c9",
+  },
+  fonts: {
+    // Todo add fonts
+  }
+};
